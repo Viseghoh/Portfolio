3ba493540cc43a308172cafa6cb62c706dafae4b
Viseghoh <vidal.iseghohimen@gmail.com>
2021-03-02 22:07:11 -0700

    Game Logic Initial Setup

--- NumberGuessingGame/js/script.js
+++ NumberGuessingGame/js/script.js
@@ -1,1 +1,57 @@
 "use strict";
+let answer = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let message = document.querySelector(".message");
+let number = document.querySelector(".number");
+let highScore = document.querySelector(".highscore");
+let guessCheck = document.querySelector(".check");
+let currentScore = document.querySelector(".score");
+
+document.querySelector(".check").addEventListener("click", (e) => {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    message.textContent = "⛔️ No Number!";
+  } else if (guess === answer) {
+    message.textContent = "😍 You're Correct!";
+    number.textContent = guess;
+    highScore.textContent = score;
+    guessCheck.disabled = true;
+    document.body.style.backgroundColor = "#60b347";
+  } else if (guess > answer) {
+    if (score > 1) {
+      message.textContent = "👎🏾 Too High!";
+      score--;
+      currentScore.textContent = score;
+    } else {
+      message.textContent = "Game Over!!!";
+      guessCheck.disabled = true;
+      score = 0;
+      currentScore.textContent = score;
+      number.textContent = answer;
+    }
+  } else if (guess < answer) {
+    if (score > 1) {
+      message.textContent = "👍🏾 Too Low!";
+      score--;
+      currentScore.textContent = score;
+    } else {
+      message.textContent = "Game Over!!!";
+      guessCheck.disabled = true;
+      score = 0;
+      currentScore.textContent = score;
+      number.textContent = answer;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", (e) => {
+  guessCheck.disabled = false;
+  number.textContent = "?";
+  document.querySelector(".guess").value = "";
+  answer = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  console.log(answer);
+  currentScore.textContent = score;
+  message.textContent = "Start guessing...";
+  document.body.style.backgroundColor = "#222";
+});
