49c7e7a42a02af68b9e98a0d9119b1be958c1b00
Viseghoh <vidal.iseghohimen@gmail.com>
2021-03-03 13:23:08 -0700

    Little More Refactoring

--- NumberGuessingGame/js/script.js
+++ NumberGuessingGame/js/script.js
@@ -3,60 +3,66 @@ let answer = Math.trunc(Math.random() * 20) + 1;
 console.log(answer);
 let score = 20;
 
-let message = document.querySelector(".message");
-let number = document.querySelector(".number");
 let highScore = document.querySelector(".highscore");
-let guessCheck = document.querySelector(".check");
+
 let currentScore = document.querySelector(".score");
+
+const displayMessage = (message) => {
+  document.querySelector(".message").textContent = message;
+};
+
+const setNumber = (number) => {
+  document.querySelector(".number").textContent = number;
+};
+
+const setScore = (score) => {
+  document.querySelector(".score").textContent = score;
+};
+
+const disableCheckButton = (flag) => {
+  document.querySelector(".check").disabled = flag;
+};
+
+const setBackgroundColor = (color) => {
+  document.body.style.backgroundColor = color;
+};
 
 document.querySelector(".check").addEventListener("click", (e) => {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
-    message.textContent = "⛔️ No Number!";
+    displayMessage("⛔️ No Number!");
   } else if (guess === answer) {
-    message.textContent = "😍 You're Correct!";
-    number.textContent = guess;
-    guessCheck.disabled = true;
-    document.body.style.backgroundColor = "#60b347";
+    displayMessage("😍 You're Correct!");
+    setNumber(guess);
+    disableCheckButton(true);
+    setBackgroundColor("#60b347");
 
     if (Number(currentScore.textContent) > Number(highScore.textContent)) {
       highScore.textContent = currentScore.textContent;
     }
-  } else if (guess > answer) {
+  } else if (guess !== answer) {
     if (score > 1) {
-      message.textContent = "👎🏾 Too High!";
       score--;
-      currentScore.textContent = score;
+      displayMessage(guess > answer ? "👎🏾 Too High!" : "👍🏾 Too Low!");
+      setScore(score);
     } else {
-      message.textContent = "Game Over!!!";
-      guessCheck.disabled = true;
+      displayMessage("Game Over!!!");
+      disableCheckButton(true);
       score = 0;
-      currentScore.textContent = score;
-      number.textContent = answer;
-    }
-  } else if (guess < answer) {
-    if (score > 1) {
-      message.textContent = "👍🏾 Too Low!";
-      score--;
-      currentScore.textContent = score;
-    } else {
-      message.textContent = "Game Over!!!";
-      guessCheck.disabled = true;
-      score = 0;
-      currentScore.textContent = score;
-      number.textContent = answer;
+      setNumber(answer);
     }
   }
 });
 
 document.querySelector(".again").addEventListener("click", (e) => {
-  guessCheck.disabled = false;
-  number.textContent = "?";
+  disableCheckButton(false);
+  setNumber("?");
   document.querySelector(".guess").value = "";
   answer = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   console.log(answer);
-  currentScore.textContent = score;
-  message.textContent = "Start guessing...";
-  document.body.style.backgroundColor = "#222";
+
+  setScore(score);
+  displayMessage("Start guessing...");
+  setBackgroundColor("#222");
 });
