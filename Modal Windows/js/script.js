1ade566d876c9beecba6a49570f8943e895af4b2
Viseghoh <vidal.iseghohimen@gmail.com>
2021-03-05 08:47:32 -0700

    Added Global Event Listener

--- Modal Windows/js/script.js
+++ Modal Windows/js/script.js
@@ -1,2 +1,38 @@
 // Modal Setup
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const addGlobalEventListener = (type, selector, callback) => {
+  document.addEventListener(type, (e) => {
+    if (e.target.matches(selector)) {
+      callback(e);
+    } else if (e.key === selector) {
+      callback(e);
+    }
+  });
+};
+
+addGlobalEventListener("click", ".overlay", () => {
+  closeModal();
+});
+addGlobalEventListener("keydown", "Escape", () => {
+  closeModal();
+});
+
+addGlobalEventListener("click", ".close-modal", () => {
+  closeModal();
+});
+addGlobalEventListener("click", ".show-modal", () => {
+  openModal();
+});
